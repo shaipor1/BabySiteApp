f198d946c57408a49c8a5b413b048c7324376c32
User <User@LAB2-6>
2021-11-08 14:40:04 +0200

    sign up

--- BabySiteApp/ViewModels/SignUpViewModels.cs
+++ BabySiteApp/ViewModels/SignUpViewModels.cs
@@ -13,6 +13,78 @@ namespace BabySiteApp.ViewModels
     }
     class SignUpViewModels:BaseViewModels
     {
+        #region Name
+        private bool showNameError;
+        public bool ShowNameError
+        {
+            get => showNameError;
+            set
+            {
+                showNameError = value;
+                OnPropertyChanged("ShowNameError");
+            }
+        }
+        private string nameError;
 
+        public string NameError
+        {
+            get => nameError;
+            set
+            {
+                nameError = value;
+                OnPropertyChanged("NameError");
+            }
+        }
+        private void ValidateFName()
+        {
+            this.ShowNameError = string.IsNullOrEmpty(FirstName);
+        }
+        private void ValidateLName()
+        {
+            this.ShowNameError = string.IsNullOrEmpty(LastName);
+        }
+        private string firstName;
+        public string FirstName
+        {
+            get => firstName;
+            set
+            {
+                firstName = value;
+                ValidateFName();
+                OnPropertyChanged("FirstName");
+            }
+        }
+        private string lastName;
+        public string LastName
+        {
+            get => lastName;
+            set
+            {
+                lastName = value;
+                ValidateLName();
+                OnPropertyChanged("LastName");
+            }
+        }
+        #endregion
+        private string gender;
+        public string Gender
+        {
+            get => gender;
+            set
+            {
+                gender = value;
+                OnPropertyChanged("Gender");
+            }
+        }
+        #region email
+        private string email;
+        public string Email
+        {
+            get => email;
+            set
+            {
+                email = value;
+            }
+        }
     }
 }
